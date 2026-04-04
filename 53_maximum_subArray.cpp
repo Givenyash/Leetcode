fd4404bde14e2730e35a2a19c8b3fa2169711743
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-04 21:28:51 +0530

    maximum subarray

---
+++ 53_maximum_subArray.cpp
@@ -0,0 +1,15 @@
+int maxSubArray(vector<int>& nums) {
+        int currentsum=0;
+        int maxsum=nums[0];
+        for(int i=0; i<nums.size() ; i++){
+            currentsum= currentsum + nums[i];
+
+            if(currentsum > maxsum){
+                maxsum=currentsum;
+            }
+            if(currentsum<0){
+                currentsum=0;
+            }
+        }
+        return maxsum;
+    }
