918fc695a131e6b79f2b3f8b2ef365e460ecd8a5
Yash Gautam <yash.ishu.barehi@gmail.com>
2026-07-04 05:02:52 +0530

    Time: 6 ms (58.73%), Space: 12.4 MB (16.37%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cpp
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int dayOfYear(string date) {
-        int y = stoi(date.substr(0,4));
-        int m = stoi(date.substr(5,2));
-        int d = stoi(date.substr(8,2));
+        int y = stoi(date.substr(0, 4));
+        int m = stoi(date.substr(5, 2));
+        int d = stoi(date.substr(8, 2));
 
-        vector<int> daysOfMonths ({31,28,31,30,31,30,31,31,30,31,30,31});
+        int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-        if(isLeapYear(y)){
-            daysOfMonths[1] = 29;
+        if (y%4==0 && y!=1900){
+            arr[1] = 29;
         }
 
-        for(int i=0; i<m-1; i++){
-            d = d + daysOfMonths[i];
+        int sum = 0;
+        for (int i=0; i<m-1; i++){
+            sum += arr[i];
         }
 
-        return d;
-    }
-
-    bool isLeapYear(int y){
-       return ((y % 400 == 0) or (y%4 == 0 and y%100 != 0));
+        return sum + d;
     }
 };
