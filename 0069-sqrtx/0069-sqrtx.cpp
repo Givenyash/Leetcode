8367d1c4dd5fc42aefa47e66e6b4938673174004
Yash Gautam <yash.ishu.barehi@gmail.com>
2026-07-20 23:43:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.49%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+       int mySqrt(int x){
+
+    if (x == 0 || x == 1){
+        return x;
+    }
+
+    long long start = 1;
+    long long end = x;
+    
+    while(start <= end){
+        long long mid = start + (end-start)/2;
+
+        if(mid*mid > x){
+            end = mid - 1;
+        }
+        else if(mid*mid == x){
+            return mid;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return end;  
+}
+};
