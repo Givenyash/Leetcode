dcb13426a84a3a42473cd3311dce1ee88decc012
Yash Gautam <yash.ishu.barehi@gmail.com>
2026-07-31 02:44:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.71%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -6,12 +6,8 @@ public:
         int res = 0;
 
         if(n<=8) return n;
-        if(n>8 &&n<=16){
-            return 8 + ((n-8)*2);
-        }
-        if(n>16 && n<=24){
-              return 24 + ((n-16)*3);
-        }
+        if(n>8 &&n<=16) return 8 + ((n-8)*2);
+        if(n>16 && n<=24) return 24 + ((n-16)*3);
         return 48 + ((n-24)*4);
     }
 };
