80e1f32f1892e220ebc6a68629509d238b2e81ed
Yash Gautam <yash.ishu.barehi@gmail.com>
2026-08-05 23:28:43 +0530

    Time: 28 ms (47.57%), Space: 41.7 MB (89.33%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -3,15 +3,11 @@ public:
     int mostFrequentEven(vector<int>& nums) {
         unordered_map<int,int>mp;
         int n = nums.size();
-
         for(int x : nums){
-            if(x % 2 == 0){
-                mp[x]++;
-            }
+            if(x % 2 == 0) mp[x]++;
         }
         int max = 0;
         int ans = -1;
-
         for(auto it : mp){
             int num = it.first;
             int freq = it.second;
