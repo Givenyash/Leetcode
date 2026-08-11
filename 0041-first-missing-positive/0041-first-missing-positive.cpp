a32c932614e7e0352e9324e310dc01c135abc4b0
Yash Gautam <yash.gautam.global@gmail.com>
2026-08-11 21:42:37 +0530

    Time: 63 ms (11.19%), Space: 78.2 MB (15.17%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i = 1;
+        unordered_map<int,int>mp;
+
+        for(int x : nums){
+            mp[x]++;
+        }
+
+        while(mp.contains(i)){
+            i++;
+        }
+        return i;
+    }
+};
