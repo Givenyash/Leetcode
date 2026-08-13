587e5f51e026f163ee1363c2b8c8013f6659be5b
Yash Gautam <yash.gautam.global@gmail.com>
2026-08-13 17:26:58 +0530

    Time: 4 ms (4.68%), Space: 9.3 MB (42.94%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+       unordered_map<char, int>mp;
+       for(int x : s) mp[x]++;
+
+       int count = 0;
+       bool oddFound = false;
+
+       for(auto j : mp){
+            if(j.second % 2 == 0){
+                count += j.second;
+            }
+            else{
+                count = count + j.second - 1;
+                oddFound = true;
+            }
+        }
+        if(oddFound) count++;
+        return count;
+    }
+};
