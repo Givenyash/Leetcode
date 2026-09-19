ac4f7417bd35ffd326701e8fb5b7ed39fc124a94
Yash Gautam <yash.gautam.global@gmail.com>
2026-09-19 15:59:54 +0530

    Time: 21 ms (41.69%), Space: 28.3 MB (77%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res = "";
+
+        for(char c : s){
+            if(c == '*'){
+                if(!res.empty()){
+                    res.pop_back();
+                }
+            }
+            else{
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
