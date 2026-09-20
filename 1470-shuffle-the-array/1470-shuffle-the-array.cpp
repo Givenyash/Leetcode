cc4375332453646832d3f12821a80c06a72fa325
Yash Gautam <yash.gautam.global@gmail.com>
2026-09-20 23:55:54 +0530

    Time: 4 ms (65.19%), Space: 13.3 MB (97.32%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+       int z = nums.size();
+
+       int left = 0;
+       int right = z / 2;
+
+       vector<int>ans(2*n);
+
+        for(int i=0; i<n; i++){
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[i+n];
+        }
+        return ans;
+    }
+};
